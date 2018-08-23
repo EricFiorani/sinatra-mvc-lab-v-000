943cebd761bb8620be43fc5512a8e753b3d8f7b6
Eric Fiorani <efiorani305@gmail.com>
2018-08-23 20:08:07 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,10 +1,26 @@
 class PigLatinizer
 
-  attr_reader :words, :word
-  
   def piglatinize(word)
-
+    result = ""
+    if word[0].downcase =~ /[aeiou]/
+      result = word + "way"
+    else
+      index = first_vowel(word)
+      result = word[index..-1] + word[0..(index-1)] + "ay"
+    end
   end
 
+  def multiple_words_to_pig_latin(text)
+    text.split.collect do |word|
+      piglatinize(word)
+    end.join(" ")
+  end
 
+  def first_vowel(word)
+    word.downcase.split(//).each_with_index do |letter, i|
+      if letter =~ /[aeiou]/
+        return i
+      end
+    end
+  end
 end
