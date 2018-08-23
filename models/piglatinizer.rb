d9350496306de5f2d563f4d02f828f100edf4abf
Eric Fiorani <efiorani305@gmail.com>
2018-08-23 20:24:07 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,19 +5,15 @@ class PigLatinizer
   end
 
   def piglatinize_word(word)
-    #Starts with a vowel?
     if !consonant?(word[0])
       word = word + "w"
-
-    #Starts with 3 consonants??
+      
     elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
       word = word.slice(3..-1) + word.slice(0,3)
-
-    #2 consonants?
+      
     elsif consonant?(word[0]) && consonant?(word[1])
       word = word.slice(2..-1) + word.slice(0,2)
-
-    #1 consonant? =O
+      
     else
       word = word.slice(1..-1) + word.slice(0)
     end
@@ -30,9 +26,7 @@ class PigLatinizer
 
   def piglatinize(user_input)
     x = (user_input.split(" ").length == 1) ? piglatinize_word(user_input) : piglatinize_phrase(user_input)
-
     puts x
-
     x
   end
 
