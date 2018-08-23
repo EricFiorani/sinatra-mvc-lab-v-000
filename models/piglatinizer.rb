23840a95f7452553e6a6641e081a4f6e4f90403f
Eric Fiorani <efiorani305@gmail.com>
2018-08-23 19:58:27 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,6 +1,20 @@
 class PigLatinizer
 
-  def piglatinize(word)
+  def piglatinize(words)
+    words=word.split(" ")
+    words.each do |x|
+    if ["a","e","i","o","u"].include?x[0,1]
+      x << ("ay")
+    else
+      until ["a","e","i","o","u"].include?x[0,1]
+        x << ("#{x[0,1]}")
+        x[0,1]=""
+      end
+      x << "ay"
+      end
+    end
+    words.join(" ")
   end
+
 
 end
